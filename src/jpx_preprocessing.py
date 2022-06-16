5d1ded1a7fb78c2e31e97943225e0f1f6f07e3df
Jianan Tang <36015544+tojunesa@users.noreply.github.com>
2022-06-15 20:53:14 -0400

    Add files via upload

---
+++ src/jpx_preprocessing.py
@@ -0,0 +1,191 @@
+import pandas as pd
+import numpy as np
+import os
+import collections
+import csv
+
+def separate_stocks(read_path, save_path):
+    '''
+    Input:
+        read_path (str) where to read train/supplemental files
+        save_path (str) where to save the loaded price info for every stock
+    Output:
+        security_code_dict (dictionary) security code -> number of trading days
+    '''
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+        
+    df = pd.read_csv(read_path)
+    df.drop(labels=['AdjustmentFactor','ExpectedDividend', 'SupervisionFlag'], axis=1, inplace=True)
+    df.drop(labels = ['RowId', 'Open', 'High', 'Low', 'Volume'], axis=1, inplace=True)
+    df.sort_values(['SecuritiesCode', 'Date'], inplace = True)
+    
+    security_code_dict = {}
+    longest_days = 0
+    length = df.shape[0]
+    start = 0
+    end = 0
+    security_code = df.iloc[start]['SecuritiesCode']
+    for i in range(length):
+        if df.iloc[i]['SecuritiesCode']!=security_code:
+            end = i
+            security_code_dict[security_code] = end-start
+            temp_df = pd.DataFrame(df.iloc[start:end])
+            temp_df.to_csv(save_path+str(security_code)+'.csv', index = False)
+            start = i
+            security_code = df.iloc[start]['SecuritiesCode']
+
+    security_code_dict[security_code] = length-start
+    temp_df = pd.DataFrame(df.iloc[start:])
+    temp_df.to_csv(save_path+str(security_code)+'.csv', index = False)
+    
+    return security_code_dict
+
+def feature_processing(save_path, security_code_dict):
+    '''
+    Input:
+        path: (str) folder where the raw data (for each stock) are stored
+    '''
+    for security_code in security_code_dict.keys():
+        df = pd.read_csv(save_path+str(security_code)+'.csv')
+        df['pClose'] = df['Close'].shift(1)
+        df['Return_Close'] = (df['Close'] - df['pClose'])/df['pClose'] 
+        df.fillna(0, inplace = True)
+        df.to_csv(save_path+str(security_code)+'.csv', index=False)
+
+def fill_date(file_name, template_df):
+    '''
+    Input:
+        file_name: (str) path to the file that need to be processed
+        template_df: (dataframe) longest stock info dataframe
+    '''
+    temp_df = template_df.copy(deep=True)
+    df = pd.read_csv(file_name)
+    len_df = df.shape[0]
+    p1 = 0
+    p2 = 0
+    while(p1<temp_df.shape[0] and p2<len_df):
+        if df.iloc[p2]['Date'] == temp_df.iloc[p1]['Date']:
+            temp_df.iloc[p1, df.columns.get_loc('Return_Close')] = df.iloc[p2]['Return_Close']
+            #temp_df.iloc[p1, df.columns.get_loc('Return_Open')] = df.iloc[p2]['Return_Open']
+            #temp_df.iloc[p1, df.columns.get_loc('Return_High')] = df.iloc[p2]['Return_High']
+            #temp_df.iloc[p1, df.columns.get_loc('Return_Low')] = df.iloc[p2]['Return_Low']
+            temp_df.iloc[p1, df.columns.get_loc('Target')] = df.iloc[p2]['Target']
+            #temp_df.iloc[p1, df.columns.get_loc('Volume')] = df.iloc[p2]['Volume']
+            p1+=1
+            p2+=1
+        else:
+            temp_df.iloc[p1, df.columns.get_loc('Return_Close')] = 0
+            #temp_df.iloc[p1, df.columns.get_loc('Return_Open')] = 0
+            #temp_df.iloc[p1, df.columns.get_loc('Return_High')] = 0
+            #temp_df.iloc[p1, df.columns.get_loc('Return_Low')] = 0
+            temp_df.iloc[p1, df.columns.get_loc('Target')] = 0
+            #temp_df.iloc[p1, df.columns.get_loc('Volume')] = 0
+            p1+=1
+    while(p1<temp_df.shape[0]):
+        temp_df.iloc[p1, df.columns.get_loc('Return_Close')] = 0
+        #temp_df.iloc[p1, df.columns.get_loc('Return_Open')] = 0
+        #temp_df.iloc[p1, df.columns.get_loc('Return_High')] = 0
+        #temp_df.iloc[p1, df.columns.get_loc('Return_Low')] = 0
+        temp_df.iloc[p1, df.columns.get_loc('Target')] = 0
+        #temp_df.iloc[p1, df.columns.get_loc('Volume')] = 0
+        p1+=1
+    temp_df.to_csv(file_name, index = False)
+
+def fill_date_batch(path, security_code_dict):
+    '''
+        Input:
+            path (str) folder where the processed (for each stock) are stored 
+            security_code_dict (dictionary) security code -> number of trading days
+    '''
+    longest_stock = max(list(security_code_dict.values()))
+    stocks_need_filling = []
+    for key, val in security_code_dict.items():
+        if val < longest_stock: stocks_need_filling.append(key)
+        if val == longest_stock: template_stock = key
+
+    template_df = pd.read_csv(path+str(template_stock)+'.csv')
+    for security_code in stocks_need_filling:
+        file_name = path+str(security_code)+'.csv'
+        fill_date(file_name, template_df)
+
+def concate_train_and_sup_files(train_path, sup_path, all_path, security_code_dict):
+    for secu_code in security_code_dict.keys():
+        train_df = pd.read_csv(train_path+str(secu_code)+'.csv')
+        sup_df = pd.read_csv(sup_path+str(secu_code)+'.csv')
+        all_df = pd.concat([train_df, sup_df], axis=0)
+        all_df.to_csv(all_path+str(secu_code)+'.csv',index = False)
+
+def group_by_sectors(stock_list_path, data_path, write_path, sector_path, isTraining):
+    '''
+        stock_list_path (str) stock_list.csv path
+        data_path (str) stock data path
+        write_path (str) output directory
+        sector_path (str) directory to save sector information (security codes within a sector)
+        isTraining (bool) is this processing the traning data
+    '''
+    sector = {}
+    with open(stock_list_path, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            sector[row[0]] = row[7]
+    
+    sec_list = os.listdir(data_path)
+    
+    sector_list = collections.defaultdict(list)
+    for sec_code in sec_list:
+        sector_list[sector[sec_code[:-4]]].append(sec_code[:-4])
+    
+    for key, lst in sector_list.items():
+        lst.sort(key = lambda x: int(x))
+        feature = None
+        target = None
+        for sec_code in lst:
+            df = pd.read_csv(data_path+sec_code+'.csv')
+            if feature is None:
+                #feature = df[['Return_Close', 'Return_Open', 'Return_High', 'Return_Low', 'Volume']]
+                feature = df[['Return_Close']]
+                if isTraining: target = df['Target']
+            else:
+                #feature = pd.concat([feature, df[['Return_Close', 'Return_Open', 'Return_High', 'Return_Low', 'Volume']]], axis=1)
+                feature = pd.concat([feature, df[['Return_Close']]], axis=1)
+                if isTraining: target = pd.concat([target, df['Target']], axis=1)
+        feature.to_csv(write_path+'sector_x_'+key+'.csv', index=False)
+        if isTraining: target.to_csv(write_path+'sector_y_'+key+'.csv', index=False)
+      
+        pd.DataFrame(lst).to_csv(sector_path+str(key)+'.csv', index=False, header=False)
+        
+        
+def preprocessing():
+    train_path = 'input/jpx-tokyo-stock-exchange-prediction/train_files/stock_prices.csv'
+    sup_path = 'input/jpx-tokyo-stock-exchange-prediction/supplemental_files/stock_prices.csv'
+    train_save_path = 'working/train/'+ 'stocks/'
+    sup_save_path = 'working/sup/' + 'stocks/'
+    stock_list_path = 'input/jpx-tokyo-stock-exchange-prediction/stock_list.csv'
+    
+    all_path = 'working/all/'
+    all_stock_path = 'working/all/' + 'stocks/'
+    all_sector_path = 'working/all/' + 'sectors/'
+    
+    sector_path = 'working/sector_list/'
+
+    if not os.path.exists(train_save_path): os.makedirs(train_save_path)
+    if not os.path.exists(sup_save_path): os.makedirs(sup_save_path)
+
+    if not os.path.exists(all_path): os.makedirs(all_path)
+    if not os.path.exists(sector_path): os.makedirs(sector_path)
+
+    if not os.path.exists(all_stock_path): os.makedirs(all_stock_path)
+    if not os.path.exists(all_sector_path): os.makedirs(all_sector_path)
+
+    train_security_code_dict = separate_stocks(train_path, train_save_path)
+    sup_security_code_dict = separate_stocks(sup_path, sup_save_path)
+
+    feature_processing(train_save_path, train_security_code_dict)
+    feature_processing(sup_save_path, sup_security_code_dict)
+
+    fill_date_batch(train_save_path, train_security_code_dict)
+    fill_date_batch(sup_save_path, sup_security_code_dict)
+    concate_train_and_sup_files(train_save_path, sup_save_path, all_stock_path, train_security_code_dict)
+
+    group_by_sectors(stock_list_path, all_stock_path, all_sector_path, sector_path, True)
---
+++ src/main.py
@@ -0,0 +1,109 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import jpx_utils
+import jpx_preprocessing
+import tensorflow.keras.backend as kb
+import tensorflow as tf
+print('import success!')
+
+def run_MLP(xfile, yfile, sec_code_file, sector, window=5):
+    '''
+    Input:
+        xfile: (str) path to dataset input feature
+        yfile: (str) path to dataset target
+        sec_code_file: (str) path to the file where stock security numbers are stored
+        sector: (str) sector where the model is trained
+        window (str): (int) lenght of time window
+    Output:
+        yPr: (np array) test days*number of stocks
+        sec_code_list: (np array of str) security numbers of the stocks
+    '''
+    offset = window-1
+    xTr, yTr, sec_code_list = jpx_utils.dataloader(xfile, yfile, sec_code_file)
+    num_stock = len(sec_code_list[0])
+    ema5Tr, ema10Tr, ema20Tr = jpx_utils.MovingAverage(xTr)
+    xTr, yTr = jpx_utils.FormTimeWindow(xTr, yTr, window)
+    xTr = np.concatenate((xTr, ema5Tr[offset:], ema10Tr[offset:], ema20Tr[offset:]), axis=1)
+    xVa, yVa = xTr[-60:], yTr[-60:]
+    xTr, yTr = xTr[:-60], yTr[:-60]
+    
+    #print(xTr.shape, yTr.shape, xTe.shape, yTe.shape, sec_code_list.shape)
+
+    #mlp__first_layer_nodes = [xTr.shape[1]*2],
+    if os.path.exists('working'): return xVa, yVa
+    model = jpx_utils.mlp_model(input_dims=xTr.shape[1],
+                    output_dims=yTr.shape[1],
+                    n_layers=10,
+                    first_layer_nodes=min(num_stock*4*window,10000),
+                    last_layer_nodes=num_stock*2,
+                    activation_func='relu',
+                    loss_func='binary_crossentropy')
+    #print(model.summary())
+    history = model.fit(xTr, yTr, batch_size = 50, epochs = 40, verbose = 0, shuffle=True)
+    model.save('/home/fs01/yy692/Kaggle/Kaggle_TokyoStock/working/models/'+sector+'_model')
+
+    kb.clear_session()
+    return xVa, yVa
+
+def RunValidation(xVa_lst, yVa_df, sector_stock_mapping):
+    eps = 1e-5
+    weights = np.array([1.0+(1.0/199.0)*i for i in range(200)][::-1])
+    relt, rest_list = None, None
+    for idx, xVa in enumerate(xVa_lst):
+        model = tf.keras.models.load_model('/home/fs01/yy692/Kaggle/Kaggle_TokyoStock/working/models/'+str(idx+1)+'_model')
+        yPr = model.predict(xVa)
+
+        output, rest = jpx_utils.FindPair(yPr, sector_stock_mapping[idx])
+        if relt is None:
+            relt = output
+        else:
+            relt = np.concatenate((relt,output), axis=1)
+
+        if rest is None: continue
+        if rest_list is None:
+            rest_list = rest
+        else:
+            rest_list = np.concatenate((rest_list, rest), axis=1)
+
+    rank = jpx_utils.WholeRank(relt, rest_list)
+    gains = []
+    for day in range(len(rank)):
+        topGain = np.sum(yVa_df.iloc[day].loc[rank[day][:200]].to_numpy()*weights)
+        botGain = np.sum(yVa_df.iloc[day].loc[rank[day][-200:]].to_numpy()*np.flip(weights))
+        gains.append(topGain - botGain)
+    gains = np.array(gains)
+
+    return np.mean(gains)/(np.std(gains)+eps)
+ 
+
+if __name__ == "__main__":
+    if not os.path.exists('all'): jpx_preprocessing.preprocessing()
+
+    sectors = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17']
+    sector_path = '/home/fs01/yy692/Kaggle/Kaggle_TokyoStock/working/sector_list/'
+    path = '/home/fs01/yy692/Kaggle/Kaggle_TokyoStock/working/all/sectors/'
+    sector_stock_mapping = []
+    
+    for sec in sectors:
+        sec_code_file = sector_path+sec+".csv"
+        sec_code_list = pd.read_csv(sec_code_file, header = None, dtype=str).to_numpy()
+        sector_stock_mapping.append(np.squeeze(sec_code_list))
+    
+    xVa_lst, yVa_df = [], None
+    for sec in sectors:
+        xfile = path+'sector_x_'+sec+'.csv'
+        yfile = path+'sector_y_'+sec+'.csv'
+        sec_code_file = sector_path+sec+".csv"
+        xVa, yVa = run_MLP(xfile, yfile, sec_code_file, sec, window=5)
+        xVa_lst.append(xVa.copy())
+
+        tmp_df = pd.DataFrame(yVa)
+        tmp_df.columns = sector_stock_mapping[int(sec)-1]
+        if yVa_df is None: 
+            yVa_df = tmp_df.copy()
+        else:
+            yVa_df = pd.concat([yVa_df, tmp_df.copy()], axis=1)
+        #yVa_lst.append(yVa.copy())
+
+    print(RunValidation(xVa_lst, yVa_df, sector_stock_mapping))
